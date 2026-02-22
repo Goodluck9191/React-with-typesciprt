4e3e4ea5c5fbc1ecbc89581a4bf5700864ad8645
Goodluck <goodluckprosper76@gmail.com>
2026-02-22 14:47:53 +0300

    Button component with typescript

---
+++ src/component/Input.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Input = () => {
+  const [email, setEmail] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Your email is ${email}`);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Enter your email</label>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Input;
